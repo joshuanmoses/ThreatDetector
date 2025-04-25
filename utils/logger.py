f14802598b61f70eea01e6c600119ee0148d39f2
Joshua Moses <59664518+joshuanmoses@users.noreply.github.com>
2025-04-25 14:39:29 -0500

    Update logger.py

--- utils/logger.py
+++ utils/logger.py
@@ -1,1 +1,13 @@
+import logging
+
+def setup_logger(name):
+    formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+    handler = logging.StreamHandler()
+    handler.setFormatter(formatter)
+    
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.INFO)
+    logger.addHandler(handler)
+    
+    return logger
 
