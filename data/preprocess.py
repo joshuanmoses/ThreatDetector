f1ddee5698081a17e7a5930f5bbbc6ae7616d302
Joshua Moses <59664518+joshuanmoses@users.noreply.github.com>
2025-04-25 14:46:06 -0500

    Update preprocess.py

--- data/preprocess.py
+++ data/preprocess.py
@@ -1,1 +1,8 @@
+import pandas as pd
+
+def preprocess_session_data(raw_data):
+    df = pd.DataFrame(raw_data)
+    df['duration'] = df['end_time'] - df['start_time']
+    features = df[['duration', 'command_count', 'resource_accessed_count']]
+    return features
 
